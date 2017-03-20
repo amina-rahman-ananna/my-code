5f7f7e9d377609c2d89fa6e24b1b046b4ea2ed2e
Amina Rahman Ananna <aminaananna@yahoo.com>
2017-03-20 12:41:57 +0600

    Create linklist visit.cpp

---
+++ DS/linklist visit.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+ class List
+ {
+   public:
+       int a;
+
+       List *next;
+ };
+
+ List *head =NULL;
+ void Create(int x)
+ {
+
+     List *temp =NULL;
+     for(int i=1;i<=x;i=i+1)
+     {
+         if(i==1)
+         {
+             head=new List();
+             cin >> head->a;
+             temp=head;
+         }
+         else
+         {
+             List *q = new List();
+             cin >> q->a;
+             temp->next =q;
+             temp =q;
+         }
+          temp->next=NULL;
+     }
+
+ }
+
+ void visit()
+ {
+     List *c=head;
+     do
+     {
+         cout << c->a << " ";
+         c= c-> next;
+
+     }
+     while(c!=NULL);
+ }
+ int main ()
+ {
+    Create(3);
+    visit();
+     return 0;
+
+ }
