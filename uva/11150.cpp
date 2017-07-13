af3dda7f7bfe70dfc58731cc214ca5e2472876fb
Amina Rahman Ananna <aminaananna@yahoo.com>
2017-07-13 13:37:25 +0600

    Create 11150.cpp

---
+++ uva/11150.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+ int main()
+ {
+   int a,n;
+   while (cin >> n)
+   {
+     a=n;
+     while (n>=3)
+     {
+       a=a+n/3;
+       n= (n/3) + (n%3);
+       
+     }
+     if(n==2) a++;
+     cout << a << endl;
+   }
+  return 0;
+}
