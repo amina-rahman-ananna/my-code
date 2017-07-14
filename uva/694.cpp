a26cb4984209052989c81301e2b929f8f9f39a76
Amina Rahman Ananna <aminaananna@yahoo.com>
2017-07-14 20:55:11 +0600

    The Collatz Sequence.cpp

---
+++ uva/694.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+long long a,b,c,i,j=1;
+while(scanf("%lld%lld",&b,&c)==2)
+{
+if(b<0 && c<0)
+break;
+a=b;
+for(i=1;i!=0;i++)
+    {
+    if(a==1)
+    break;
+    if(a%2==0)
+    a=a/2;
+    else
+    a=a*3+1;
+    if(a>c)
+    break;
+    }
+
+printf("Case %lld: A = %lld, limit = %lld, number of terms = %lld\n",j,b,c,i);
+j++;
+}
+}
